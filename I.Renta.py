f51a2151d4cadf273dae0ad22adac8f96abf8817
Abogradro <programapork@gmail.com>
2026-08-12 17:18:21 -0500

    Refactor income tax calculation with user input
    Updated the income tax calculation to use user input for annual income and UIT value.

--- I.Renta.py
+++ I.Renta.py
@@ -1,16 +1,12 @@
-# Un trabajador proyecta un ingreso anual bruto de 55000 soles.
-# Resta 7 UIT (5350 cada una) para obtener la renta_neta_imponible.
-# Si la renta_neta_imponible es <= 0, el impuesto es 0 soles.
-# Si la renta_neta_imponible <= (5 * 5350), aplica 8%, sino 14% e imprime impuesto.
+ingreso_anual = float (input ("Hola, indica tu ingreso anual por favor:   ") )
+uit = int (input ("Indica el valor de la UIT este año; el valor de la UIT de este año 2026 es de S/. 5350:   ")) 
 
-ingreso_anual = 55000
-uit = 5350
 renta_neta_imponible = ingreso_anual - (7*uit)
 
 if renta_neta_imponible <= 0:
     impuesto = 0
 elif renta_neta_imponible <= (5*uit):
-    impuesto = ingreso_anual * 0.08
+    impuesto = renta_neta_imponible * 0.08
 else:
     impuesto = renta_neta_imponible * 0.14
 
